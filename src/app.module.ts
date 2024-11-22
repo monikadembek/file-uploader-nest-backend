9ca9a0053b403a82897c64be1055a52ac644094c
Monika <monika.dembek@gmail.com>
2024-11-22 18:54:35 +0100

    feat: implement uploading image files to cloudinary

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,17 @@ import { AppService } from './app.service';
 import { FileUploadModule } from './file-upload/file-upload.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..'),
     }),
     FileUploadModule,
+    CloudinaryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
